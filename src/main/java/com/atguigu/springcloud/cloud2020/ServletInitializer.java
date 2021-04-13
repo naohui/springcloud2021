2a4350c71e92996fe468f2142c547a5951069bfa
ruixue <ruixue@ncsi.com.cn>
2021-04-13 19:28:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/springcloud/cloud2020/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.cloud2020;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Cloud2020Application.class);
+	}
+
+}
